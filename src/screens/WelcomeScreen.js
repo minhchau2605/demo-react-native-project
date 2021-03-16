ad64c1e72409fa977820b8df5cf56758b61b5ef1
chaudm7053 <chaudm7053@co-well.com.vn>
2021-03-16 14:52:56 +0700

    v1

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,43 +1,81 @@
-import * as React from 'react';
-import {View, StyleSheet, ImageBackground, Image, Text} from 'react-native';
+import React, {Component} from 'react';
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function WelcomeScreen() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoView}>
-        <Image
-          source={require('../assets/images/logo-b-w.png')}
-          style={styles.image}
-        />
-        <Text style={styles.welcome}>Chào mừng</Text>
-        <Text style={styles.welcome} numberOfLines={5}>bạn đến với chúng tôi</Text>
-      </View>
-    </View>
-  );
+export default class OnBoardScreen extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <ImageBackground
+              style={styles.background}
+              source={require('../assets/images/logo-b-w.png')}>
+            <View style={styles.overlay}>
+              <Image
+                  style={styles.icon}
+                  source={{
+                    uri:
+                        'https://s3-alpha-sig.figma.com/img/5ad4/936d/fe4ebbdbc8a462e62c18c41765e852dd?Expires=1616976000&Signature=WXmR9Q-3oO0Mod3-dGLG3SMX~SrdITi4ke-Y~KPGMhatjh-b0ELVBESUdUcoY~2onHZzF4eRpSD7mLe-3NTT7J2cFBBbzyjbHEzj0YLgHfWU~84BPmzvWKWYvFmw9addF2OfNxZmWsRwhf0hi847KCFqhblLP~IytTij~zMvfcC3ExVVsTPIscR0~f3vAGMKNZtttvFCHFYJWpuXq85kibPH3CmXzjArmN2eNdAgAedtikdz4dGkgMZUotKIgpKcT340rDDsy2bN1pLYwDOGW1m0c56TWwoa2iLBa7UQJ5mBnkIwXL0in3otppVxa0DZ3d3PfLJ-YmKKE6eZqcRgWQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+                  }}
+              />
+              <Text numberOfLines={2} ellipsizeMode={'tail'} style={styles.title}>
+                Chào mừng {'\n'}đến với chúng tôi
+              </Text>
+              <TouchableOpacity style={styles.button} onPress={this.onPress}>
+                <Text style={styles.textButton}>Bắt đầu</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#004318',
-    opacity: 0.8,
-  },
-  logoView: {
-    marginTop: 30,
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
-    width: 161,
-    height: 93,
+  background: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    resizeMode: 'cover',
+    alignItems: 'center',
   },
-  welcome: {
-    fontFamily: 'Actor',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: 26,
-    lineHeight: 29,
-    color: '#FFFFFF',
+  icon: {width: 144, height: 83},
+  overlay: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,67,24,0.9)',
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    paddingTop: 22,
+    textAlign: 'center',
+    fontSize: 36,
+    color: 'white',
+    paddingBottom: 79,
+  },
+  button: {
+    borderRadius: 20,
+    width: 350,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#DDDDDD',
+  },
+  textButton: {
+    color: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
   },
 });
